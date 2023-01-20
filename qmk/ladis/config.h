1a6ffa8a3e33734f44012052ca54284cfd94b181
Alexander Zhuravlev <alex15023@yandex.ru>
2023-01-20 23:24:56 +0300

    Latest qmk

--- qmk/ladis/config.h
+++ qmk/ladis/config.h
@@ -3,21 +3,12 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Ladis
-#define PRODUCT         LadisPad
-#define DESCRIPTION     macropad
-
 /* key matrix size */
 #define MATRIX_ROWS 1
 #define MATRIX_COLS 11
 
 /* key matrix pins */
 #define DIRECT_PINS { { D3, B1, D4, C6, D7, E6, B4, B5, F4, F7, B3 } }
-#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -25,11 +16,11 @@
 /* number of backlight levels */
 
 #ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 0
+#define BACKLIGHT_LEVELS 1
 #endif
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCING 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
@@ -42,12 +33,7 @@
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
 
-/* prevent stuck modifiers */
-#define PREVENT_STUCK_MODIFIERS
-
-
 #ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 0
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
