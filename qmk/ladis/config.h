74b0aba15487a3c56b73599767263101ee410a32
Alexander Zhuravlev <alex15023@yandex.ru>
2023-12-11 00:31:03 +0300

    fixes for new release

--- qmk/ladis/config.h
+++ qmk/ladis/config.h
@@ -1,46 +1,20 @@
-#ifndef CONFIG_H
-#define CONFIG_H
+// Copyright 2023 Alexander Zhuravlev (@Alexander Zhuravlev)
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "config_common.h"
+#pragma once
 
-/* key matrix size */
-#define MATRIX_ROWS 1
-#define MATRIX_COLS 11
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
 
-/* key matrix pins */
-#define DIRECT_PINS { { D3, B1, D4, C6, D7, E6, B4, B5, F4, F7, B3 } }
+/* disable debug print */
+//#define NO_DEBUG
 
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
+/* disable print */
+//#define NO_PRINT
 
-/* number of backlight levels */
-
-#ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 1
-#endif
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCING 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
-
-#ifdef RGB_DI_PIN
-#define RGBLED_NUM 0
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-#endif
-
-#endif
-
-#define ENCODERS_PAD_A { F5, B6 }
-#define ENCODERS_PAD_B { F6, B2 }
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
--- qmk/ladis/keymaps/via/keymap.c
+++ qmk/ladis/keymaps/via/keymap.c
@@ -1,85 +1,83 @@
-//#include "ladis.h"
+
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include QMK_KEYBOARD_H
 
 #define _BL 0
 #define _FL 1
 
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [_BL] =   { ENCODER_CCW_CW(KC_BRIGHTNESS_UP, KC_BRIGHTNESS_DOWN), ENCODER_CCW_CW(KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN)  },
+};
+#endif
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-[_BL] = LAYOUT(
-		KC_WWW_BACK, KC_UP, KC_WWW_FORWARD, \
-		KC_LEFT, KC_DOWN, KC_RIGHT, \
-		MO(_FL), KC_WWW_REFRESH, KC_F16, \
-		LCTL(KC_LGUI), KC_MUTE),
-
-[_FL] = LAYOUT(
-		KC_F17, KC_F18, KC_F19, \
-		KC_F20, KC_F21, KC_F22, \
-		KC_NO, KC_F23, KC_F24, \
-		KC_SYSTEM_SLEEP, KC_MUTE),
-
-[2] = LAYOUT(
-		KC_TRNS , KC_TRNS , KC_TRNS , \
-		KC_TRNS , KC_TRNS , KC_TRNS , \
-		KC_TRNS , KC_TRNS , KC_TRNS , \
-		KC_TRNS , KC_TRNS ),
-
-[3] = LAYOUT(
-		KC_TRNS , KC_TRNS , KC_TRNS , \
-		KC_TRNS , KC_TRNS , KC_TRNS , \
-		KC_TRNS , KC_TRNS , KC_TRNS , \
-		KC_TRNS , KC_TRNS ),
-
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │   │
+     * ├───┼───┼───┤ + │
+     * │ 4 │ 5 │ 6 │   │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │   │
+     * ├───┴───┼───┤Ent│
+     * │   0   │ . │   │
+     * └───────┴───┴───┘
+     */
+    [_BL] = LAYOUT(
+		KC_Q, KC_W, KC_E,
+		KC_A, KC_S, KC_D, KC_UP,
+		KC_Z, KC_X, KC_C, KC_DOWN
+    )
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) { /* First encoder */
-        if (clockwise & layer_state_is(_BL)) {
-            tap_code(KC_RIGHT);
-        } else if (clockwise & layer_state_is(_FL)){
-			tap_code(KC_MS_WH_DOWN);
+        if (clockwise) {
+            tap_code(KC_BRIGHTNESS_UP);
+        } else {
+			tap_code(KC_BRIGHTNESS_DOWN);
         }
-		else if (!clockwise & layer_state_is(_BL)){
-            tap_code(KC_LEFT);
-        }
-		 else if (!clockwise & layer_state_is(_FL)) {
-            tap_code(KC_MS_WH_UP);
-		}
     } else if (index == 1) { /* Second encoder */
-        if (clockwise & layer_state_is(_BL)) {
-            tap_code(KC_AUDIO_VOL_DOWN);
-        } else if (clockwise & layer_state_is(_FL)){
-			tap_code(KC_MS_WH_RIGHT);
-        }
-
-		else if (!clockwise & layer_state_is(_BL)){
+        if (clockwise) {
             tap_code(KC_AUDIO_VOL_UP);
-        }
-		else if (!clockwise & layer_state_is(_FL)) {
-            tap_code(KC_MS_WH_LEFT);
+        } else {
+            tap_code(KC_AUDIO_VOL_DOWN);
 		}
     }
     return false;
 }
 
-static void render_logo(void) {
-    static const char PROGMEM raw_logo[] = {
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 31,127,255,255,255,255,255, 31, 15,  7,  7,  7,  3,  3,  3,  3,  3,  7,  7,  7, 15, 63,255,255,255,255,255,255,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,128,224,224,240,112,112,240,240,224,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,192,240,240,192,  0,  0,128,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  3,127,255,255,255,255,254,252,252,252,252,124, 56,124,252,252,252,254,254,254,255,255,255,255, 63,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  7,  7,  1,192,248,255,255, 15,  1,128,192,224,224,240,112,176, 32,  0,  0,  0,128,192,224,240,112, 48, 32,224,252,255,127,  7,192,240,231,  7,  7,224,224,240, 48, 48,240,224, 64,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,128,255,255,255,255,255,223,135,135,191,248,112,112,187,135,  7,135,143,255,255,255,255,251,128,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,192,192,224,252,255,191,135,  0, 16,254,255,199,227,112, 60,254,255, 63,  6,  0,126,255,255,195,240, 56,124,255,255, 31,  3, 32,252,255, 63,  7,112,240,225,199,207,255,252,120,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,248,255,255,255,255,255,255,255,255,255,253,253,253,252,252,253,255,255,255,255,255,255,255,255,255,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  3,  3,  7,  7, 15, 14, 30, 30, 28, 28, 14,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    };
-    oled_write_raw_P(raw_logo, sizeof(raw_logo));
-}
-
-
+#ifdef OLED_ENABLE
 bool oled_task_user(void) {
-  if (is_keyboard_master()) {
-    // If you want to put your image on the master side, put your function call here:
-    render_logo();
-  } else {
-    // And if you want to put your image on the slave side, put it here instead:
-    render_logo();
-  }
-  return true;
+    oled_write_ln_P(PSTR("Hello, Vovan"), false);
+
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            oled_write_P(PSTR("Default\n"), false);
+            break;
+        case 1:
+            oled_write_P(PSTR("Numbers\n"), false);
+            break;
+        case 2:
+            oled_write_P(PSTR("ADJ\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+
+    return false;
 }
+#endif
+
